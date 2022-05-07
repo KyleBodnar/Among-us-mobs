784b5d51290e042ac2a1e0c446f39ca9b8d658b7
KyleBodnar <kylebodnar12358@gmail.com>
2022-05-07 13:29:17 -0400

    + amogium block

--- src/main/java/com/invalid/amongusmobs/block/BlockRegister.java
+++ src/main/java/com/invalid/amongusmobs/block/BlockRegister.java
@@ -9,8 +9,10 @@ import net.minecraft.util.registry.Registry;
 
 public class BlockRegister {
     public static final Block AMOGIUM_ORE = new Block(FabricBlockSettings.of(Material.STONE).strength(4.0f));
+    public static final Block AMOGIUM_BLOCK = new Block(FabricBlockSettings.of(Material.STONE).luminance(8).strength(4.0f));
 
     public static void registerBlocks() {
         Registry.register(Registry.BLOCK, new Identifier(Amogus.MOD_ID, "amogium_ore"), AMOGIUM_ORE);
+        Registry.register(Registry.BLOCK, new Identifier(Amogus.MOD_ID, "amogium_block"), AMOGIUM_BLOCK);
     }
 }
