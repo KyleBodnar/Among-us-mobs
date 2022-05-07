704541f62acfabe998d7f0900205f78d52e01441
KyleBodnar <kylebodnar12358@gmail.com>
2022-05-07 12:02:29 -0400

    + custom item group for mod items

--- src/main/java/com/invalid/amongusmobs/Amogus.java
+++ src/main/java/com/invalid/amongusmobs/Amogus.java
@@ -4,7 +4,11 @@ package com.invalid.amongusmobs;
 import com.invalid.amongusmobs.block.BlockRegister;
 import com.invalid.amongusmobs.item.ItemRegister;
 import com.invalid.amongusmobs.tools.ToolRegister;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ToolItem;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import net.fabricmc.api.ModInitializer;
@@ -13,9 +17,15 @@ public class Amogus implements ModInitializer {
     public static final String MOD_ID = "amongusmobs";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+    public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(
+            new Identifier(MOD_ID, "amongusgroup"),
+            () -> new ItemStack(ItemRegister.AMOGUS_TOTEM));
+
     @Override
     public void onInitialize() {
         LOGGER.info("the sussy is loaded");
+
+
         ItemRegister.registerItems();
         BlockRegister.registerBlocks();
         ToolRegister.registerTools();
--- src/main/java/com/invalid/amongusmobs/item/ItemRegister.java
+++ src/main/java/com/invalid/amongusmobs/item/ItemRegister.java
@@ -11,14 +11,14 @@ import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
 
 public class ItemRegister {
-    public static final Item KEYCARD = new Item(new FabricItemSettings().group(ItemGroup.TOOLS));
-    public static final AmogusTotem AMOGUS_TOTEM = new AmogusTotem(new FabricItemSettings().group(ItemGroup.MISC).fireproof().rarity(Rarity.EPIC));
-    public static final Item AMOGIUM_RAW = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS));
-    public static final Item AMOGIUM_INGOT = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS));
+    public static final Item KEYCARD = new Item(new FabricItemSettings().group(Amogus.ITEM_GROUP));
+    public static final AmogusTotem AMOGUS_TOTEM = new AmogusTotem(new FabricItemSettings().group(Amogus.ITEM_GROUP).fireproof().rarity(Rarity.EPIC));
+    public static final Item AMOGIUM_RAW = new Item(new FabricItemSettings().group(Amogus.ITEM_GROUP));
+    public static final Item AMOGIUM_INGOT = new Item(new FabricItemSettings().group(Amogus.ITEM_GROUP));
 
     public static void registerItems() {
         Registry.register(Registry.ITEM, new Identifier(Amogus.MOD_ID, "keycard"), KEYCARD);
-        Registry.register(Registry.ITEM, new Identifier(Amogus.MOD_ID, "amogium_ore"), new BlockItem(BlockRegister.AMOGIUM_ORE, new FabricItemSettings().group(ItemGroup.MISC)));
+        Registry.register(Registry.ITEM, new Identifier(Amogus.MOD_ID, "amogium_ore"), new BlockItem(BlockRegister.AMOGIUM_ORE, new FabricItemSettings().group(Amogus.ITEM_GROUP)));
         Registry.register(Registry.ITEM, new Identifier(Amogus.MOD_ID, "amogus_totem"), AMOGUS_TOTEM);
         Registry.register(Registry.ITEM, new Identifier(Amogus.MOD_ID, "amogium_raw"), AMOGIUM_RAW);
         Registry.register(Registry.ITEM, new Identifier(Amogus.MOD_ID, "amogium_ingot"), AMOGIUM_INGOT);
