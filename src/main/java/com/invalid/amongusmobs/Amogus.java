cba1735e729acd139575ea0fdcb86dbedf3e1f7c
KyleBodnar <kylebodnar12358@gmail.com>
2022-05-09 06:54:26 -0400

    + deleted broken files

--- src/main/java/com/invalid/amongusmobs/Amogus.java
+++ src/main/java/com/invalid/amongusmobs/Amogus.java
@@ -8,11 +8,12 @@ import com.invalid.amongusmobs.tools.ToolRegister;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ToolItem;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import net.fabricmc.api.ModInitializer;
+import software.bernie.geckolib3.GeckoLib;
+
 public class Amogus implements ModInitializer {
 
     public static final String MOD_ID = "amongusmobs";
@@ -25,11 +26,12 @@ public class Amogus implements ModInitializer {
     @Override
     public void onInitialize() {
         LOGGER.info("the sussy is loaded");
-
+        GeckoLib.initialize();
 
         ItemRegister.registerItems();
         BlockRegister.registerBlocks();
         ToolRegister.registerTools();
         EnchantRegister.registerEnchants();
+
     }
 }
